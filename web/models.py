e88de71b67e771c35876c0b33ee73dfaa7957dac
2460307574@qq.com <2460307574@qq.com>
2023-04-22 10:48:53 +0800

    'index图片替换为本地'

--- web/models.py
+++ web/models.py
@@ -183,71 +183,71 @@ class Username(models.Model):
 	# account=models.DecimalField(null=True,verbose_name='用户表密码' ,max_digits=10,decimal_places=2) #精准数据类型 最大数位 精确到2位
 
 	time=models.DateTimeField(null=True,verbose_name='时间',auto_now_add=True)#时间戳
-class Google_search_picture(models.Model):
-    """
-    CharField 类型必须设置 max_length 参数
-    verbose_name 是对字段的解释,每个字段都有,且是第一个默认形参,所以可以直接在第一个位置实参处写值不需要写verbose_name=
-    null=True 允许字段为空
-    default=0 设置默认值
-    """
-    # id int primary key auto_increment;(sql语句效果与下面orm语句相同)
-    id = models.AutoField(primary_key=True,verbose_name="主键ID")
-    # name varchar(32);
-    title = models.TextField(null=True,verbose_name="标题")
-    #图片url
-    pic1_url = models.TextField(null=True,verbose_name="图片url")
-    pic1_loc_img = models.TextField(null=True,verbose_name="标签图片url")
-    pic2_url = models.TextField(null=True,verbose_name="图片url")
-    pic3_url = models.TextField(null=True,verbose_name="图片url")
-    pic4_url = models.TextField(null=True,verbose_name="图片url")
-    pic5_url = models.TextField(null=True,verbose_name="图片url")
-    pic6_url = models.TextField(null=True,verbose_name="图片url")
-    #图片位置
-    pic1_loc = models.TextField(null=True,verbose_name="图片位置")
-    pic2_loc = models.TextField(null=True,verbose_name="图片位置")
-    pic3_loc = models.TextField(null=True,verbose_name="图片位置")
-    pic4_loc = models.TextField(null=True,verbose_name="图片位置")
-    pic5_loc = models.TextField(null=True,verbose_name="图片位置")
-    pic6_loc = models.TextField(null=True,verbose_name="图片位置")
-    #搜图查到的图url
-    goods1_pic_url = models.TextField(null=True,verbose_name="搜图查到的图url")
-    goods2_pic_url = models.TextField(null=True,verbose_name="搜图查到的图url")
-    goods3_pic_url = models.TextField(null=True,verbose_name="搜图查到的图url")
-    goods4_pic_url = models.TextField(null=True,verbose_name="搜图查到的图url")
-    goods5_pic_url = models.TextField(null=True,verbose_name="搜图查到的图url")
-    goods6_pic_url = models.TextField(null=True,verbose_name="搜图查到的图url")
-    goods7_pic_url = models.TextField(null=True,verbose_name="搜图查到的图url")
-    goods8_pic_url = models.TextField(null=True,verbose_name="搜图查到的图url")
-    goods9_pic_url = models.TextField(null=True,verbose_name="搜图查到的图url")
-    goods10_pic_url = models.TextField(null=True,verbose_name="搜图查到的图url")
-    #找到的10个产品url
-    goods1_url = models.TextField(null=True,verbose_name="找到的10个产品url")
-    goods2_url = models.TextField(null=True,verbose_name="找到的10个产品url")
-    goods3_url = models.TextField(null=True,verbose_name="找到的10个产品url")
-    goods4_url = models.TextField(null=True,verbose_name="找到的10个产品url")
-    goods5_url = models.TextField(null=True,verbose_name="找到的10个产品url")
-    goods6_url = models.TextField(null=True,verbose_name="找到的10个产品url")
-    goods7_url = models.TextField(null=True,verbose_name="找到的10个产品url")
-    goods8_url = models.TextField(null=True,verbose_name="找到的10个产品url")
-    goods9_url = models.TextField(null=True,verbose_name="找到的10个产品url")
-    goods10_url = models.TextField(null=True,verbose_name="找到的10个产品url")
-    #查到的标题
-    goods1_title = models.TextField(null=True,verbose_name="查到的标题")
-    goods2_title = models.TextField(null=True,verbose_name="查到的标题")
-    goods3_title = models.TextField(null=True,verbose_name="查到的标题")
-    goods4_title = models.TextField(null=True,verbose_name="查到的标题")
-    goods5_title = models.TextField(null=True,verbose_name="查到的标题")
-    goods6_title = models.TextField(null=True,verbose_name="查到的标题")
-    goods7_title = models.TextField(null=True,verbose_name="查到的标题")
-    goods8_title = models.TextField(null=True,verbose_name="查到的标题")
-    goods9_title = models.TextField(null=True,verbose_name="查到的标题")
-    goods10_title = models.TextField(null=True,verbose_name="查到的标题")
-    #已经搜图
-    search_picture = models.TextField(null=True,verbose_name="已经搜图")
-    #更新时间
-    time=models.DateTimeField(null=True,auto_now=True)
-    #创建时间(只在记录创建时变动,然后不可修改)
-    create_time=models.DateTimeField(null=True,auto_now_add=True)
+# class Google_search_picture(models.Model):
+#     """
+#     CharField 类型必须设置 max_length 参数
+#     verbose_name 是对字段的解释,每个字段都有,且是第一个默认形参,所以可以直接在第一个位置实参处写值不需要写verbose_name=
+#     null=True 允许字段为空
+#     default=0 设置默认值
+#     """
+#     # id int primary key auto_increment;(sql语句效果与下面orm语句相同)
+#     id = models.AutoField(primary_key=True,verbose_name="主键ID")
+#     # name varchar(32);
+#     title = models.TextField(null=True,verbose_name="标题")
+#     #图片url
+#     pic1_url = models.TextField(null=True,verbose_name="图片url")
+#     pic1_loc_img = models.TextField(null=True,verbose_name="标签图片url")
+#     pic2_url = models.TextField(null=True,verbose_name="图片url")
+#     pic3_url = models.TextField(null=True,verbose_name="图片url")
+#     pic4_url = models.TextField(null=True,verbose_name="图片url")
+#     pic5_url = models.TextField(null=True,verbose_name="图片url")
+#     pic6_url = models.TextField(null=True,verbose_name="图片url")
+#     #图片位置
+#     pic1_loc = models.TextField(null=True,verbose_name="图片位置")
+#     pic2_loc = models.TextField(null=True,verbose_name="图片位置")
+#     pic3_loc = models.TextField(null=True,verbose_name="图片位置")
+#     pic4_loc = models.TextField(null=True,verbose_name="图片位置")
+#     pic5_loc = models.TextField(null=True,verbose_name="图片位置")
+#     pic6_loc = models.TextField(null=True,verbose_name="图片位置")
+#     #搜图查到的图url
+#     goods1_pic_url = models.TextField(null=True,verbose_name="搜图查到的图url")
+#     goods2_pic_url = models.TextField(null=True,verbose_name="搜图查到的图url")
+#     goods3_pic_url = models.TextField(null=True,verbose_name="搜图查到的图url")
+#     goods4_pic_url = models.TextField(null=True,verbose_name="搜图查到的图url")
+#     goods5_pic_url = models.TextField(null=True,verbose_name="搜图查到的图url")
+#     goods6_pic_url = models.TextField(null=True,verbose_name="搜图查到的图url")
+#     goods7_pic_url = models.TextField(null=True,verbose_name="搜图查到的图url")
+#     goods8_pic_url = models.TextField(null=True,verbose_name="搜图查到的图url")
+#     goods9_pic_url = models.TextField(null=True,verbose_name="搜图查到的图url")
+#     goods10_pic_url = models.TextField(null=True,verbose_name="搜图查到的图url")
+#     #找到的10个产品url
+#     goods1_url = models.TextField(null=True,verbose_name="找到的10个产品url")
+#     goods2_url = models.TextField(null=True,verbose_name="找到的10个产品url")
+#     goods3_url = models.TextField(null=True,verbose_name="找到的10个产品url")
+#     goods4_url = models.TextField(null=True,verbose_name="找到的10个产品url")
+#     goods5_url = models.TextField(null=True,verbose_name="找到的10个产品url")
+#     goods6_url = models.TextField(null=True,verbose_name="找到的10个产品url")
+#     goods7_url = models.TextField(null=True,verbose_name="找到的10个产品url")
+#     goods8_url = models.TextField(null=True,verbose_name="找到的10个产品url")
+#     goods9_url = models.TextField(null=True,verbose_name="找到的10个产品url")
+#     goods10_url = models.TextField(null=True,verbose_name="找到的10个产品url")
+#     #查到的标题
+#     goods1_title = models.TextField(null=True,verbose_name="查到的标题")
+#     goods2_title = models.TextField(null=True,verbose_name="查到的标题")
+#     goods3_title = models.TextField(null=True,verbose_name="查到的标题")
+#     goods4_title = models.TextField(null=True,verbose_name="查到的标题")
+#     goods5_title = models.TextField(null=True,verbose_name="查到的标题")
+#     goods6_title = models.TextField(null=True,verbose_name="查到的标题")
+#     goods7_title = models.TextField(null=True,verbose_name="查到的标题")
+#     goods8_title = models.TextField(null=True,verbose_name="查到的标题")
+#     goods9_title = models.TextField(null=True,verbose_name="查到的标题")
+#     goods10_title = models.TextField(null=True,verbose_name="查到的标题")
+#     #已经搜图
+#     search_picture = models.TextField(null=True,verbose_name="已经搜图")
+#     #更新时间
+#     time=models.DateTimeField(null=True,auto_now=True)
+#     #创建时间(只在记录创建时变动,然后不可修改)
+#     create_time=models.DateTimeField(null=True,auto_now_add=True)
     # # age int;
     # age = models.IntegerField(null=True,verbose_name="年龄")
     # # pwd int;
